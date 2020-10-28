f58999996a8cbfe8559f0737548690bec91943cb
Igor P. Armelin <igor_armelin@hotmail.com>
2020-10-28 12:11:42 -0300

    exclusão index.html

---
+++ exercício-10/script.js
@@ -0,0 +1,4 @@
+/* 
+O método forEach tem a função de iterar cada item de um array.
+Já o método Map, além de iterar cada item de um array, ele também tem o 'poder' de alterar cada item iterado.
+*/
